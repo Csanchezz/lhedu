fd24a0c16f941215c84202e9c8a8f27183b0d063
Csanchezz <csanchezz@thinkandmake.net>
2017-01-27 00:14:27 -0600

    reconstruction of structure of the program
    cambié todo como está el del suni

--- src/src/settings.py
+++ src/src/settings.py
@@ -13,21 +13,40 @@ SECRET_KEY = '$06me@7zp)8)59-ifk(x1^p3dic6$9^we^ic8f_f51m2q%!29)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'braces',
+    'easy_thumbnails',
+    'dynamic_preferences',
+    'widget_tweaks',
+    'menu',
+    'dal',
+    'dal_select2',
+    'mixer',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
+
+
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -39,13 +58,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lhedu.urls'
+ROOT_URLCONF = 'src.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -59,7 +79,12 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lhedu.wsgi.application'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+WSGI_APPLICATION = 'src.wsgi.application'
 
 
 # Database
@@ -95,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-es'
 
 TIME_ZONE = 'UTC'
 
@@ -114,3 +139,61 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+MEDIA_ROOT = 'etc/media/'
+MEDIA_URL = '/media/'
+
+THUMBNAIL_ALIASES = {
+    '': {
+        'background': {'size': (1350, 650), 'crop': True},
+        'avatar-lg': {'size': (500, 375), 'crop': True},
+        'avatar': {'size': (180, 180), 'crop': True},
+        'avatar-sm': {'size': (90, 90), 'crop': True},
+        'icon': {'size': (40, 40), 'crop': True},
+        'icon-xs': {'size': (15, 15), 'crop': True},
+    },
+}
+
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/users/profile/'
+
+# account settings
+ACCOUNT_EMAIL_REQUIRED = True
+SOCIALACCOUNT_AUTO_SIGNUP = False
+ACCOUNT_ADAPTER = 'allauth.account.adapter.DefaultAccountAdapter'
+ACCOUNT_SIGNUP_FORM_CLASS = 'apps.users.forms.CustomSignupForm'
+ACCOUNT_SIGNUP_PASSWORD_VERIFICATION = False
+
+
+DYNAMIC_PREFERENCES = {
+
+    # a python attribute that will be added to model instances with preferences
+    'MANAGER_ATTRIBUTE': 'preferences',
+
+    # The python module in which registered preferences will be searched within each app
+    'REGISTRY_MODULE': 'dynamic_preferences_registry',
+
+    # Allow quick editing of preferences directly in admin list view
+    'ADMIN_ENABLE_CHANGELIST_FORM': False,
+
+    # Should we enable the admin module for user preferences ?
+    'ENABLE_USER_PREFERENCES': True,
+
+    # Customize how you can access preferences from managers. The default is to
+    # separate sections and keys with two underscores. This is probably not a settings you'll
+    # want to change, but it's here just in case
+    'SECTION_KEY_SEPARATOR': '__',
+
+    # Use this to disable caching of preference. This can be useful to debug things
+    'ENABLE_CACHE': True,
+
+    # Use this to disable checking preferences names. This can be useful to debug things
+    'VALIDATE_NAMES': True,
+}
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "lcontreras@funsepa.org"
+EMAIL_HOST_PASSWORD = "Funsepa253"
+EMAIL_USE_TLS = True
+EMAIL_FROM = "lcontreras@funsepa.org"
+EMAIL_REPLY_TO = EMAIL_FROM
